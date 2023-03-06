d0bf87e2470ed43145a1ed260e816e3016b604a1
David Ayrolla <davidcadastros@gmail.com>
2023-03-06 12:35:41 -0300

    Add Membership.Inline in Artwork and Person.

--- core/admin.py
+++ core/admin.py
@@ -2,14 +2,30 @@ from django.contrib import admin
 from .models import *
 
 
+class MembershipInline(admin.TabularInline):
+    model = Membership
+
+
+class ArtworkAdmin(admin.ModelAdmin):
+    inlines = [
+        MembershipInline,
+    ]
+
+
+class PersonAdmin(admin.ModelAdmin):
+    inlines = [
+        MembershipInline,
+    ]
+
+
 admin.site.register(Country)
 admin.site.register(Role)
-admin.site.register(Person)
+admin.site.register(Person, PersonAdmin)
 admin.site.register(Distributor)
 admin.site.register(Genre)
 admin.site.register(Language)
 admin.site.register(TypeOfArtwork)
-admin.site.register(Artwork)
+admin.site.register(Artwork, ArtworkAdmin)
 admin.site.register(Membership)
 admin.site.register(UserProfile)
 
