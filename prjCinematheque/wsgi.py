9e965a21ba61b725ba7ca814891bb2b8c7f7ccee
David Ayrolla <davidcadastros@gmail.com>
2023-03-15 17:53:26 -0300

    Funcionando em debug (ver depois como retirar debug)

--- prjCinematheque/wsgi.py
+++ prjCinematheque/wsgi.py
@@ -8,12 +8,8 @@ https://docs.djangoproject.com/en/4.1/howto/deployment/wsgi/
 """
 
 import os
-
-
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'prjCinematheque.settings')
 
-from dj_static import Cling
-# application = get_wsgi_application()
-application = Cling(get_wsgi_application())
+application = get_wsgi_application()
