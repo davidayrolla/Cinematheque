18451f1ef1caf806058eb177fef4b63eb04befe2
David Ayrolla <davidcadastros@gmail.com>
2023-03-15 13:43:39 -0300

    404 page

--- prjCinematheque/settings.py
+++ prjCinematheque/settings.py
@@ -13,7 +13,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-9aa+!3=1eugf=ua%=sqjw5wmz4$a%u3-)%*q0y5oi(uziqb9t2'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['localhost',
                  '127.0.0.1',
