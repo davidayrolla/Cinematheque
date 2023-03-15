3d99cef44a95e52e0e695616a3349dd1db9be36b
David Ayrolla <davidcadastros@gmail.com>
2023-03-15 12:30:20 -0300

    Deploy config.

--- prjCinematheque/settings.py
+++ prjCinematheque/settings.py
@@ -1,6 +1,7 @@
 import os
 from pathlib import Path
 import django_heroku
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -10,10 +11,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-9aa+!3=1eugf=ua%=sqjw5wmz4$a%u3-)%*q0y5oi(uziqb9t2'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG', default=False, cast=bool)
 
 ALLOWED_HOSTS = ['localhost',
                  '127.0.0.1',
