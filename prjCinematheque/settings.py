92c129612653c47cf88831a86b0d111c3d9cdd0c
David Ayrolla <davidcadastros@gmail.com>
2024-03-15 19:38:08 -0300

    Tentando mostrar imagens

--- prjCinematheque/settings.py
+++ prjCinematheque/settings.py
@@ -12,7 +12,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-9aa+!3=1eugf=ua%=sqjw5wmz4$a%u3-)%*q0y5oi(uziqb9t2'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*',
                  'https://cinematheque-2a3n.onrender.com/',
@@ -99,10 +99,10 @@ STATICFILES_DIRS = [
     '..' / BASE_DIR / "static",
 ]
 
-#STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-if not DEBUG:
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+#if not DEBUG:
     # Tell Django to copy static assets into a path called `staticfiles` (this is specific to Render)
-    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+    #STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
     # Enable the WhiteNoise storage backend, which compresses static files to reduce disk use
     # and renames the files with unique names for each version to support long-term caching
     #STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
