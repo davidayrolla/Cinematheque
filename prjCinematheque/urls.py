6607456665f01154fbed022751eb23b1939261c6
David Ayrolla <davidcadastros@gmail.com>
2024-03-18 18:57:32 -0300

    Corrigido problema dos media_files quando o modo de DEBUG está TRUE

--- prjCinematheque/urls.py
+++ prjCinematheque/urls.py
@@ -2,13 +2,14 @@ from django.contrib import admin
 from django.urls import path, re_path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.static import serve
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     re_path(r'^', include('core.urls') ),
     path('accounts/', include('django.contrib.auth.urls')),
+    re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
 ]
 
-if settings.DEBUG:
-     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+#urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
