8662acfa83ae9ad7215b9ef9ddb92a0af5f48f85
David Ayrolla <davidcadastros@gmail.com>
2024-03-18 11:50:14 -0300

    Testando se DUEBUG=False produz o erro das imagens.

--- prjCinematheque/settings.py
+++ prjCinematheque/settings.py
@@ -12,7 +12,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-9aa+!3=1eugf=ua%=sqjw5wmz4$a%u3-)%*q0y5oi(uziqb9t2'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*',
                  'https://cinematheque-2a3n.onrender.com/',
